1363e54afaf86ee8daefe3b832a69e119ab66705
Fiona Mellink <fimellink@gmail.com>
2024-06-27 12:31:11 +1000

    Complete lab

--- server/models.py
+++ server/models.py
@@ -11,10 +11,22 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        author = db.session.query(Author.id).filter_by(name = name).first()
+        if len(name)==0 or author is not None:
+            raise ValueError("Name cannot be blank")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self,key,phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number is not 10 digits")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -27,8 +39,32 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('title')
+    def validate_name(self, key, title):
+        if len(title)==0:
+            raise ValueError("Title cannot be blank")
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
 
+    @validates('content')
+    def valiate_content(self,key,content):
+        if len(content) <250:
+            raise ValueError("Content is too short")
+        return content
+    
+    @validates('summary')
+    def validate_content(self,key,summary):
+        if len(summary) >250:
+            raise ValueError("Summary is too long")
+        return summary
+
+    @validates('category')
+    def validate_cateory(self,key,category):
+        if category!="Fiction" and category!="Non-Fiction":
+            raise ValueError("Invalid category")
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
